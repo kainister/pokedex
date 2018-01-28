9586a14d86425afa3a7b584a2368fabe0ad1c322
Kevin Badinca <kevin.badinca@supinternet.fr>
2018-01-28 23:37:03 +0100

    added present

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 function searchPokemon(lowerPokename, pokemon, pokename) {
     var errorBlock = $('#error_block');
-    if (pokename === 'Patoche') {
-        displayPokemon(pokename, 'psychic');
-        $('#image').attr('src', 'http://paroles2chansons.lemonde.fr/lib/images/upload/artists/sebastien-patoche.jpg');
-    }else if (pokename === 'Nidoran') {
+    if (pokename === 'Patoche') { //PRESENT FOR EKITO KUN
+        displayPatoche();
+    } else if (pokename === 'Nidoran') { //PRECISE MALE OR FEMALE NIDORAN
         errorBlock.text('Please, specify: \'nidoran-m\' or \'nidoran-f\'');
     } else {
         for (var i in pokemon) {
             var name = pokemon[i].name;
             var type = pokemon[i].type;
-            if (isNaN(lowerPokename) === true && pokename === pokemon[i].name) {
-                displayPokemon(name, type);
+            if (isNaN(lowerPokename) === true && pokename === pokemon[i].name) { //IF THE POKEMON EXIST
                 errorBlock.text('');
+                displayPokemon(name, type);
                 break;
             } else {
                 errorBlock.text(pokename + ' not found.');
             }
-        }
-        if (isNaN(lowerPokename) === false) {
-            if (pokename === i) {
-                displayPokemon(name, type);
-                return false;
-            } else if (1 > pokename || 151 < pokename) {
-                errorBlock.text('pokemon number ' + pokename + ' not found, please enter a number between 1 and 151.');
-                return false;
+            if (isNaN(lowerPokename) === false) { // IF USER SEND A NUMBER
+                if ( pokename === 152) {
+                    displayPatoche();
+                    return false;
+                } else if (pokename === i) {
+                    console.log('patoche');
+                    errorBlock.text('');
+                    displayPokemon(name, type);
+                    return false;
+                } else if (1 > pokename || 152 < pokename) {
+                    errorBlock.text('pokemon number ' + pokename + ' not found, please enter a number between 1 and 151.');
+                    return false;
+                }
             }
         }
     }
 }
 
-function displayPokemon(name, type) {
+function displayPokemon(name, type) { // DISPLAY POKEMON AND THESE NAME AND TYPE
     var image = $('#image');
     var pokemonName = $('#pokemonName');
     var pokemonType = $('#pokemonType');
@@ -37,9 +41,17 @@ function displayPokemon(name, type) {
     pokemonName.text('Name : ' + name);
     pokemonType.text('Type : ' + type);
 }
+function displayPatoche() {
+    var image = $('#image');
+    var pokemonName = $('#pokemonName');
+    var pokemonType = $('#pokemonType');
+    image.attr('src', 'http://paroles2chansons.lemonde.fr/lib/images/upload/artists/sebastien-patoche.jpg');
+    pokemonName.text('Name : Patoche');
+    pokemonType.text('Type : psychic');
+}
 
 $(function () {
-    $.ajax({
+    $.ajax({ // LOAD POKEMON AND SAVE HIM IN LOCALSTORAGE
         url: 'pokemon.json',
         type: 'get',
         dataType: 'json',
@@ -49,7 +61,7 @@ $(function () {
     });
 
     $('form[name="myForm"]').submit(function () {
-        /* GET pokemon.son FROM LOCALSTORAGE */
+        /* GET pokemon.json FROM LOCALSTORAGE */
         var pokemons = localStorage.getItem('pokemons');
         var pokemon = JSON.parse(pokemons);
 
